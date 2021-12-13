376c2dc61419a1cd2549c40a2ef698e556a92b99
rihab <rihabmeddeb48@gmail.com>
2021-12-13 11:40:14 +0100

    Initial Commit

--- app/src/main/java/com/example/chatiniapp/Adapters/MessageAdapter.java
+++ app/src/main/java/com/example/chatiniapp/Adapters/MessageAdapter.java
@@ -1,7 +1,10 @@
 package com.example.chatiniapp.Adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.preference.PreferenceManager;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,7 +30,7 @@ public class MessageAdapter  extends  RecyclerView.Adapter {
 
     public MessageAdapter(Context context, List<Message> messageList) {
         this.context = context;
-        this.messageList = messageList; Toast.makeText(context, "MessageAdapter", Toast.LENGTH_SHORT).show();
+        this.messageList = messageList;
     }
 
     @Override
@@ -50,33 +53,31 @@ public class MessageAdapter  extends  RecyclerView.Adapter {
 
         View view;
 
-        //if  if (message.getSender()== PreferenceManager.getDefaultSharedPreferences(context).getInt("id", -1))  {
+        if (viewType == VIEW_TYPE_MESSAGE_SENT) {
             view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.message_sent, parent, false); Toast.makeText(context, "onCreateViewHolder", Toast.LENGTH_SHORT).show();
+                    .inflate(R.layout.message_sent, parent, false);
             return new SentMessageHolder(view);
-//        } else if (viewType == VIEW_TYPE_MESSAGE_RECEIVED) {
-//            view = LayoutInflater.from(parent.getContext())
-//                    .inflate(R.layout.message_received, parent, false);
-//            return new ReceivedMessageHolder(view);
-//        }
+        } else if (viewType == VIEW_TYPE_MESSAGE_RECEIVED) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.message_received, parent, false);
+            return new ReceivedMessageHolder(view);
+        }
 
 
-
-      //  return null;
+       return null;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         Message message = (Message) messageList.get(position);
 
-//        switch (holder.getItemViewType()) {
-//            case VIEW_TYPE_MESSAGE_SENT:
+        switch (holder.getItemViewType()) {
+            case VIEW_TYPE_MESSAGE_SENT:
                 ((SentMessageHolder) holder).bind(message);
-        Toast.makeText(context, "onBindViewHolder", Toast.LENGTH_SHORT).show();
-//                break;
-//            case VIEW_TYPE_MESSAGE_RECEIVED:
-//                ((ReceivedMessageHolder) holder).bind(message);
-       // }
+                break;
+            case VIEW_TYPE_MESSAGE_RECEIVED:
+                ((ReceivedMessageHolder) holder).bind(message);
+        }
     }
 
     @Override
@@ -109,20 +110,26 @@ public class MessageAdapter  extends  RecyclerView.Adapter {
 
     ReceivedMessageHolder(View itemView) {
         super(itemView);
-        messageText = (TextView) itemView.findViewById(R.id.text_gchat_user_other);
+        messageText = (TextView) itemView.findViewById(R.id.text_gchat_message_other);
         timeText = (TextView) itemView.findViewById(R.id.text_gchat_timestamp_other);
-        nameText = (TextView) itemView.findViewById(R.id.text_gchat_message_other);
+        nameText = (TextView) itemView.findViewById(R.id.text_gchat_user_other);
         profileImage = (ImageView) itemView.findViewById(R.id.image_gchat_profile_other);
     }
 
     void bind(Message message) {
         messageText.setText(message.getMsg());
-
+        //nameText.setText();
         // Format the stored timestamp into a readable String using method.
 //        timeText.setText(Utils.formatDateTime(message.getCreatedAt()));
 //        nameText.setText(message.getSender().getNickname());
 
         // Insert the profile image from the URL into the ImageView.
-    //    Utils.displayRoundImageFromUrl(mContext, message.getSender().getProfileUrl(), profileImage);
+//        if(user.getImg().length()!=0){
+//            byte[] imageID= android.util.Base64.decode(String.valueOf(user.getImg())   , Base64.DEFAULT);
+//            Bitmap bitmap = BitmapFactory.decodeByteArray(imageID, 0, imageID.length);
+//            holder.img.setImageBitmap(bitmap);}
+//        else
+//            holder.img.setImageDrawable(context.getResources().getDrawable(R.drawable.logo));
+
     }
 }
